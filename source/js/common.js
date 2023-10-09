d187aac3a788296105afac6c0323bfbdff151d31
jlima01 <jlima.odonto@hotmail.com>
2023-10-08 22:10:02 -0300

    Blog-V01

--- source/js/common.js
+++ source/js/common.js
@@ -1,54 +1,60 @@
-window.addEventListener('load', function() {
-    setFooter();
-    loadMEnu();
+window.addEventListener('load', function () {
+
+
+    var headertext = document.getElementById("page_header");
+    fetch("/components/header.html")
+        .then((res) => res.text())
+        .then((text) => {
+            headertext.innerHTML = text;
+        })
+        .catch((e) => console.error(e));
+
+
+    var footertext = document.getElementById("page_footer");
+    fetch("/components/footer.html")
+        .then((res) => res.text())
+        .then((text) => {
+            footertext.innerHTML = text;
+        })
+        .catch((e) => console.error(e));
+
+
+    readJson();
+
+
 })
 
+function readJson() {
+    // http://localhost:8080
 
-let setFooter =() =>{
-    footerTexts.forEach(element => {
-        let footerItem = document.createElement('span');
-        footerItem.setAttribute('class', element[1]);
-        footerItem.innerHTML = element[0] + '<br>';
-        document.getElementById('footer').appendChild(footerItem);
-    });
+    var articles = document.getElementById("articles_container");
 
-}
 
-let footerTexts = [
-    [
-        'Black-Label',
-        'bold',
-    ], [
-        'Web Development & Web Design : Juliano De Lima',
-        'footerText',
-    ], [
-        '2023',
-        'footerText',
-    ],
-];
 
-let loadMEnu = () => {
-    topMenuItems.forEach(element => {
+    fetch('/data/articles.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
 
-        let topMenuItemContent = document.createElement('div');
-        topMenuItemContent.setAttribute('class', 'topMenuItemContent');
+            var article = document.createElement('div');
 
-        let topMenuItem = document.createElement('div');
-        topMenuItem.setAttribute('class', 'topMenuItemText');
-        topMenuItem.setAttribute('id', 'topMenuItem-' + element[0]);
-        topMenuItem.setAttribute('datalink', element[1]);
-        topMenuItem.setAttribute('onclick', 'menuClickTrigger(this)');
+            json["articles"].forEach(element => {
+                console.log(element);
+                article.innerHTML = element["Data"];
 
-        let topmenuitemsvg = document.createElement('div');
-        topmenuitemsvg.setAttribute('class', 'topMenuItemIcon');
-        topmenuitemsvg.innerHTML = element[2];
 
-        topMenuItem.innerHTML = element[0];
-    
-        topMenuItemContent.append(topmenuitemsvg);
-        topMenuItemContent.append(topMenuItem);
+                articles.appendChild(article);
+            });
 
-        document.getElementById('topMenu').appendChild(topMenuItemContent);
-    });
 
-}
+
+            //console.log(this.users);
+        })
+        .catch(function () {
+            this.dataError = true;
+        })
+};
